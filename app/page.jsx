db2d2a6830de0d412431a960001ca1a4744456a2
Tabish Naqvi <tabish.naqvi2003@gmail.com>
2023-12-02 12:17:21 +0530

    added .env

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import { FaUserDoctor, FaPeopleGroup, FaBook} from "react-icons/fa6";
+
 export default function Home() {
   return (
     <section className="w-full flex justify-center flex-col items-center">
@@ -9,10 +11,22 @@ export default function Home() {
       <p className="mt-5 text-lg text-gray-600 sm:text-xl max-w-2xl font-normal  text-center">
         Cultivating resilient minds for a lifetime of happiness and nurturing mental health, one smile at a time
       </p>
-      <div>
-        <span></span>
-        <span></span>
-        <span></span>
+      <div className="flex gap-10 my-10">
+        <span className="flex flex-col items-center">
+          <span><FaUserDoctor size={20} color="#8bc63f"/></span>
+          <span className="font-semibold">Physical Counselling</span>
+          <span className="text-sm text-gray-500">in a 6 mile raduis</span>
+        </span>
+        <span className="flex flex-col items-center">
+          <span><FaPeopleGroup size={20} color="#8bc63f"/></span>
+          <span className="font-semibold">Strong Community</span>
+          <span className="text-sm text-gray-500">with 24/7 support</span>
+        </span>
+        <span className="flex flex-col items-center">
+          <span><FaBook size={20} color="#8bc63f"/></span>
+          <span className="font-semibold">Studying Modules</span>
+          <span className="text-sm text-gray-500">for absolutely free</span>
+        </span>
       </div>
     </section>
   )
